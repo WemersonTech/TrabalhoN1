0a7e25605c56087b2ddd85c9be2833ca8876ce17
Wemerson Conceição Silva <131538167+WemersonTech@users.noreply.github.com>
2024-09-10 20:59:51 -0300

    Create F_principal.c
    Problema 03 (principal)

---
+++ F_principal.c
@@ -0,0 +1,5 @@
+A função principal deverá importar, além das bibliotecas necessárias, a função implementada em 3.2.2. 
+Faça a leitura do número de casos de teste. Então, para cada quantidade de casos de teste, 
+faça a leitura da variável b e da string contendo a mensagem cifrada. 
+Converta a cada 2 caracteres para o valor decimal e imprima a mensagem. Apenas a mensagem deve ser impressa; 
+qualquer impressão além causará erros na análise automática. Lembre-se de justificar cada escolha no relatório.
