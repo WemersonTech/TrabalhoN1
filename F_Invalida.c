2c16a618a326382dba9b421d11391e5d85cfb31a
Wemerson Conceição Silva <131538167+WemersonTech@users.noreply.github.com>
2024-09-10 20:56:32 -0300

    Create F_Invalida.c
    Problema 02

---
+++ F_Invalida.c
@@ -0,0 +1,10 @@
+Implemente uma fun¸c˜ao que realize o c´alculo da fun¸c˜ao proposta conforme lib:
+
+#ifndef REMOVE_H
+#define REMOVE_H
+int func_val(int x, int b);
+#endif /* REMOVE_H */
+
+Implemente a função func_val, utilize a função round da biblioteca MATH.H para arredondar o valor do resultado
+da função.
+Se a função retornar 0, o caractere deve ser ignorado
