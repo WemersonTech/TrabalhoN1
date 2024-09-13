ce4333a265b92103d8e3c242efa0ac2218c738e1
Wemerson Conceição Silva <131538167+WemersonTech@users.noreply.github.com>
2024-09-13 02:11:07 -0300

    Update Limites.c
    Criação de Código

--- Limites.c
+++ Limites.c
@@ -1,9 +1,39 @@
-Análise dos limites suportados pelos tipos de arquivos
+//Análise dos limites suportados pelos tipos de arquivos
 
-Uma causa importante de erros em programação em C é ultrapassar os limites numéricos estabelecidos para cada tipo de variável.
-O objetivo desse exercício é mostrar como e porque ocorrem esses erros. 
-Os limites numéricos que cada tipo inteiro estão definidos na biblioteca da linguagem LIMITS.H, na forma de constantes. 
-Escreva um programa que use essa biblioteca e imprima os limites mínimos e máximos que podem ser assumidos por 
-variáveis dos tipos char, int, short int, unsigned int, long int, unsigned long int, long long int, unsigned long long int. 
-Apresente os resultados em uma tabela. Faça um pequeno programa, usando uma variável à qual é atribuída um valor maior do que seu tipo permite,
-para demonstrar o problema que pode ocorrer. Explique o que acontece se você utilizar um valor fora dos limites dos tipos de variáveis.
+#include <stdio.h>
+#include <limits.h> 
+#include <locale.h> 
+
+int main() {
+
+setlocale(LC_ALL, "Portuguese");
+     
+    printf("\tSegue abaixo, tabela de capacidades por cada tipo de variável :\n");
+    printf("+-------------------------+--------+---------------------------+---------------------------+\n");
+    printf("| Tipo                    | Bytes: |      Valor Min.:          |      Valor Max.:          |\n");
+    printf("+-------------------------+--------+---------------------------+---------------------------+\n");
+    printf("| char                    |   1    |%d                       | %d                       |\n", CHAR_MIN, CHAR_MAX);
+    printf("| unsigned char           |   1    |0                          | %u                       |\n", UCHAR_MAX);
+    printf("| short int               |   2    |%d                     | %d                     |\n", SHRT_MIN, SHRT_MAX);
+    printf("| unsigned short int      |   2    |0                          | %u                     |\n", USHRT_MAX);
+    printf("| int                     |   4    |%d                | %d                |\n", INT_MIN, INT_MAX);
+    printf("| unsigned int            |   4    |0                          | %u                |\n", UINT_MAX);
+    printf("| long int                | 4 ou 8 |%ld                | %ld                |\n", LONG_MIN, LONG_MAX);
+    printf("| unsigned long int       | 4 ou 8 |0                          | %lu                |\n", ULONG_MAX);
+    printf("| long long int           |   8    |%lld       | %lld       |\n", LLONG_MIN, LLONG_MAX);
+    printf("| unsigned long long int  |   8    |0                          | %llu      |\n", ULLONG_MAX);
+    printf("+-------------------------+--------+---------------------------+---------------------------+\n");
+    
+    
+	printf("\nObs.: \nlong int = 4 bytes (32 bits) ou 8 bytes (64 bits);\nunsigned long int = 4 bytes (32 bits) ou 8 bytes (64 bits)\n");
+	
+    
+    
+    int i = INT_MAX;
+    printf("\nValor limite de int: %d\n", i); i++;
+    printf("Valor de int +1 = overflow: %d\n\n", i);
+
+
+    return 0;
+}
+   
